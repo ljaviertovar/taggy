a7bd0a47e0929e5788f842feb5bb549653399720
Asgared <alejandro.mendiola.colin@outlook.com>
2023-03-12 14:39:59 -0600

    Add svg animation dropzone

--- src/components/ui/TaggyDropzone.tsx
+++ src/components/ui/TaggyDropzone.tsx
@@ -1,14 +1,10 @@
 import { useRef } from "react"
-import { Button, Text, Center, useMediaQuery } from "@chakra-ui/react"
-import { FaCloudUploadAlt } from "react-icons/fa"
-
+import { Button, Text, Center, useMediaQuery, Box } from "@chakra-ui/react"
 import useDropzone from "@/hooks/useDropzone"
-
 import styles from "@/styles/dropzone.module.css"
 import animations from "@/styles/animations.module.css"
 import IconTaggyAddImage from "../../assets/taggyIcons/IconTaggyAddImage"
-
-import TriangleBackground from "../../assets/taggyIcons/triangle.svg"
+import IconTaggyTriangle from "../../assets/taggyIcons/IconTaggyTriangle"
 
 interface Props {
 	actionUrl: string
@@ -22,13 +18,11 @@ export default function TaggyDropzone({ actionUrl }: Props) {
 
 	return (
 		<Center
-			backgroundImage={`url(${TriangleBackground.src})`}
-			backgroundRepeat={"no-repeat"}
-			backgroundPosition={"center"}
-			backgroundSize={"contain"}
 			w={`${isDesktop ? '420px' : '320px'}`}
 			h={`${isDesktop ? '420px' : '320px'}`}
-		>
+		>	
+			<div className={animations.gradientBorder}></div>
+			<IconTaggyTriangle />
 			<form ref={dropzoneRef} action={actionUrl} className={styles.dropzoneForm}>
 				<Button variant='unstyled' pointerEvents={"none"} w={"60px"} h={"60px"} mt={10}>
 					<IconTaggyAddImage width='60px' />
